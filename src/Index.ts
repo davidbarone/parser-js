693cb8fe9fd75eaf9d1edd5a3cb9ddb1228a5a32
davidbarone <davidbarone@live.com>
2021-06-02 21:22:04 +1000

    Symbol tests

--- src/Index.ts
+++ src/Index.ts
@@ -1,8 +1,16 @@
+import { MatchResult } from "./MatchResult";
 import { Parser } from "./Parser";
+import { RuleType } from "./RuleType";
+import { Symbol } from "./Symbol";
+
+function symbolMatchTest(pattern: string, input: string): MatchResult {
+    let s: Symbol = new Symbol(pattern, RuleType.LexerRule);
+    return s.Match(input);
+}
+
 
 // Regex tests
 let pattern = '"(?:[^"]|\\.)*"';
-let grammar = "myrule: TEST;";
-//let re = new RegExp(pattern);
-//console.log(re.test(' "This is a blah"'));
-let p = new Parser(grammar, "myrule");
+let input = '"This is some quoted text"; Rest of file;'
+console.log(symbolMatchTest(pattern, input));
+
