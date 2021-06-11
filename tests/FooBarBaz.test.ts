280360e5d5e4ed198a9ef98c1d068890f8dfa572
davidbarone <davidbarone@live.com>
2021-06-11 19:09:18 +1000

    FooBarBaz tests all pass.

--- tests/FooBarBaz.test.ts
+++ tests/FooBarBaz.test.ts
@@ -30,27 +30,18 @@ const FooBarBazVisitor = () => {
 }
 
 describe("Invalid production rule tests", () => {
-    /*
-    TestHarness.doTest("FOOBARBAZ1", FooBarBazGrammar(), "FOO", "fbb", FooBarBazVisitor(), (d) => d.items.length, 1);
-
-    DoTest("FOOBARBAZ2", FooBarBazGrammar(), "FOOBAR", "fbb", FooBarBazVisitor, (d) => d.items.Count, 2, false);
-    DoTest("FOOBARBAZ3", FooBarBazGrammar(), "FOOBARBAZ", "fbb", FooBarBazVisitor, (d) => d.items.Count, 3, false);
-    DoTest("FOOBARBAZ4", FooBarBazGrammar(), "FOOBARBAZBAZ", "fbb", FooBarBazVisitor, (d) => d.items.Count, 4, false);
-    DoTest("FOOBARBAZ5", FooBarBazGrammar(), "FOOBARBAZBAZBAZ", "fbb", FooBarBazVisitor, (d) => d.items.Count, 5, false);
-    DoTest("FOOBARBAZ6", FooBarBazGrammar(), "FOOBARBAR", "fbb", FooBarBazVisitor, (d) => d.items.Count, 3, false);
-    DoTest("FOOBARBAZ7", FooBarBazGrammar(), "FOOBARBARBAZ", "fbb", FooBarBazVisitor, (d) => d.items.Count, 4, false);
-    DoTest("FOOBARBAZ8", FooBarBazGrammar(), "FOOBARBARBARBAZ", "fbb", FooBarBazVisitor, (d) => d.items.Count, 5, false);
-    DoTest("FOOBARBAZ9", FooBarBazGrammar(), "FOOBARBAZ", "fbb", FooBarBazVisitor, (d) => d.items.Count, 3, false);
-    */
-
-
     test.each([
         ["FOOBARBAZ1", FooBarBazGrammar(), "FOO", "fbb", FooBarBazVisitor(), (d: any) => d.items.length, 1],
-        ["FOOBARBAZ2", FooBarBazGrammar(), "FOOBAR", "fbb", FooBarBazVisitor(), (d: any) => d.items.length, 2]
+        ["FOOBARBAZ2", FooBarBazGrammar(), "FOOBAR", "fbb", FooBarBazVisitor(), (d: any) => d.items.length, 2],
+        ["FOOBARBAZ3", FooBarBazGrammar(), "FOOBARBAZ", "fbb", FooBarBazVisitor(), (d) => d.items.length, 3],
+        ["FOOBARBAZ4", FooBarBazGrammar(), "FOOBARBAZBAZ", "fbb", FooBarBazVisitor(), (d) => d.items.length, 4],
+        ["FOOBARBAZ5", FooBarBazGrammar(), "FOOBARBAZBAZBAZ", "fbb", FooBarBazVisitor(), (d) => d.items.length, 5],
+        ["FOOBARBAZ6", FooBarBazGrammar(), "FOOBARBAR", "fbb", FooBarBazVisitor(), (d) => d.items.length, 3],
+        ["FOOBARBAZ7", FooBarBazGrammar(), "FOOBARBARBAZ", "fbb", FooBarBazVisitor(), (d) => d.items.length, 4],
+        ["FOOBARBAZ8", FooBarBazGrammar(), "FOOBARBARBARBAZ", "fbb", FooBarBazVisitor(), (d) => d.items.length, 5],
+        ["FOOBARBAZ9", FooBarBazGrammar(), "FOOBARBAZ", "fbb", FooBarBazVisitor(), (d) => d.items.length, 3]
     ])('%s', (name, grammar, input, rootProductionRule, visitor, resultMapping, expectedResult) => {
         var result = TestHarness.doTest(name, grammar, input, rootProductionRule, visitor, resultMapping);
         expect(result).toEqual(expectedResult);
     });
-
-
 });
